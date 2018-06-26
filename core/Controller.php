cab42ec4586e6cad47234b777d65fc397ba130fb
Felipe Acelino <felipeacelino@hotmail.com>
2018-06-25 22:43:13 -0300

    Criando controller - Parte 1

---
+++ core/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace core;
+
+use app\classes\Uri;
+
+class Controller {
+
+    private $uri;
+
+    public function __construct() {
+
+        $this->uri = Uri::uri();
+
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 
 require "../bootstrap.php";
+
+$controller = new Controller;
+$controller = $controller->getController();
+
+/* $method = new Method;
+$method = $method->getMethod();
+
+$parameters = new Parameters;
+$parameters = $parameters->getParameters();
+
+$controller->$method($parameters); */
